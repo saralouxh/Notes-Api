280ff21365b272c4f5f5ccffadff9d4b21a4ebf0
Sara Hampson <shampson518@gmail.com>
2024-01-08 21:00:16 -0600

    created database

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,20 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_14_000101) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.string "content"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "tokens", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "value"
     t.datetime "expiry"
     t.string "ip"
